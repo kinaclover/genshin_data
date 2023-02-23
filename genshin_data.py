0a340f8b55b4ffe35656912c9d09c43c63bd6deb
김희태 <kinaclover@gmail.com>
2023-02-23 17:01:07 +0900

    cookie 예제 테스트

--- genshin_data.py
+++ genshin_data.py
@@ -1,4 +1,7 @@
+import json
+import rsa
 import genshinstats
+import genshin
 import discord
 from discord.ext import commands
 # from PIL import Image ::::: 이미지 생성을 위한 라이브러리(pip install Pillow)
@@ -18,13 +21,34 @@ async def on_ready():
 async def chat(ctx):
     # embed sample code
     embed = discord.Embed(title="Test Title", colour=discord.Colour.blue())
-    embed.set_author(name="Embeded Message Test")
-    embed.set_footer(text="from 원붕이의 원신생활")
+    embed.set_author(name="Embeded Message Test - author")
+    embed.set_footer(text="from TEST - delete after 10 sec")
     # embed.set_thumbnail(url="")
     out = "{}, plz do something!".format(ctx.author.mention)
     embed.add_field(name="sample1", value=out)
-    embed.add_field(name="sample2", value="")
-    await ctx.send(embed=embed)
+    embed.add_field(name="sample2", value="Just test. don't worry.")
+    # ephemeral=True : 사용자에게만 보이도록 표시, interaction 일 경우에만 적용된다고 함. 지금은 의미없음
+    print(ctx.author)
+    await ctx.send(embed=embed, delete_after=10, ephemeral=True)
+
+
+@bot.command("getData")
+async def get_data_test(ctx):
+    uid = "801936571"
+    client = genshin.Client()
+    client.set_browser_cookies()
+
+    # login with username/password
+    # client = genshin.Client()
+    # cookies = await client.login_with_password('', '')
+    # print(cookies)
+    # {'ltoken': '', 'ltuid': '', 'cookie_token': '', 'account_id': '', 'stoken': ''}
+
+    data = await client.get_genshin_user(uid)
+    # file_path = 'test_file_2.json'
+    print(data.stats)
+    # with open(file_path, 'w', encoding='utf-8') as o:
+    #     o.write(data.stats)
 
 
 @bot.command()
@@ -56,4 +80,4 @@ async def get_spiral_data(uid):
     # print("spiral abyss data ::::: ", data.max_floor, data.total_stars)W
 
 
-bot.run("")
+bot.run("MTAzODI2MDE2ODc5NTg4OTc1NA.Gg_AKe.24JK7BguidnWkQmoYvZsnsX4AOV9wDsR1HG4rY")
